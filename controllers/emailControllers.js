c0b3cf090876566ddddbaba54ab17a609f0e5800
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-29 00:34:57 +0200

    Create the send email controller

---
+++ controllers/emailControllers.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+const nl2br = require("nl2br");
+
+const sendEmail = async (req, res) => {
+  const data = req.body;
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.user,
+        pass: process.env.password,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: `${data?.name} ${data?.surname} <${data?.email}>`,
+      to: "kacpersmyczyk@gmail.com",
+      subject: `${data?.subject} <${data?.email}>`,
+      html: `<h1>Name: ${data?.name}</h1><h1>Surname: ${
+        data?.surname
+      }</h1><h2>Email: ${data?.email}</h2><p>${nl2br(
+        data?.message
+      )}</p><br/>Embedded image: <img src=${
+        data?.attachment
+      } width="100" height="150"/>`,
+    });
+    console.dir(req.body);
+    console.log("Message sent: $s", info.messageId);
+    res.json({ response: "ok" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+    console.log("error: %s", error);
+  }
+};
+
+module.exports = {
+  sendEmail,
+};
