e81df8be7ffedec1366b513f508e86da5a272de4
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-29 00:34:44 +0200

    Create the server

---
+++ server.js
@@ -0,0 +1,14 @@
+//const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const emailRoutes = require("./routes/emailRoutes");
+
+const PORT = 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use("/api/email", emailRoutes);
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
