57889288bc4f6337509e6d9a28f362c7d896e13e
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-29 01:45:02 +0200

    Revert changes

--- controllers/emailControllers.js
+++ controllers/emailControllers.js
@@ -3,14 +3,6 @@ const nodemailer = require("nodemailer");
 const nl2br = require("nl2br");
 
 const sendEmail = async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
   const data = req.body;
   try {
     let transporter = nodemailer.createTransport({
