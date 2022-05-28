6b5f26c3d563487c04df0fce2c106ec185cfbc82
Kacper Smyczyk <smykoo7@gmail.com>
2022-05-29 01:44:53 +0200

    Change port to use env variable

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const cors = require("cors");
 const emailRoutes = require("./routes/emailRoutes");
 
-const PORT = 5000;
-
 app.use(express.json());
-app.use(cors({ origin: `*` }));
+app.use(cors({ origin: "https://ksdev.netlify.app/", credentials: true }));
 app.options("*", cors());
 app.use("/api/email", emailRoutes);
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`server running on port ${process.env.PORT}`)
+);
